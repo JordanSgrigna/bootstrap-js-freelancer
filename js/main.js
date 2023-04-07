5eca0d08c44d818c76a0c2d02fa8a9dd9f8c9548
JordanSgrigna <kaliwyrdiscord@gmail.com>
2023-04-07 09:44:50 +0200

    added form and submit function

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//----------- PROGRAMMA PRINCIPALE ------------------
+
+// Codici sconto del 25%
+let discountCodes = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+
+
+
+// -------------------------------------------
+
+
+// ----------------- FUNZIONI ------------------------
+
+//Funzione form
+function calculateWorkPrice(event){
+    event.preventDefault();
+
+    
+
+}
